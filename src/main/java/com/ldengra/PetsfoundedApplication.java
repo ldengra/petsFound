9c17f84f14ba38df7ae0c723b747bf30543c6bb8
ldengra <ldengra@hotmail.com>
2025-09-05 18:11:50 +0200

    commit
    first commit

---
+++ src/main/java/com/ldengra/PetsfoundedApplication.java
@@ -0,0 +1,13 @@
+package com.ldengra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetsfoundedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetsfoundedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ldengra/PetsfoundedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ldengra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetsfoundedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
